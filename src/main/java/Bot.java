2bad048aa02325a1274173b08de459db1e43980f
Leonardo Torres <2212.leonardo@gmail.com>
2021-03-18 20:46:41 -0300

    Hide Discord Token.

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -3,13 +3,10 @@ import org.javacord.api.DiscordApiBuilder;
 
 public class Bot {
 
-    private static String token = "ODIxODM0MjAxMDg2Njg5MzAx.YFJevg.c25qbj7K1T_o1uDfjak4fAiAoe0";
-
-    private static DiscordApi api = new DiscordApiBuilder()
-            .setToken(token)
-            .login().join();
-
     public static void main(String[] args) {
+
+        String token = new PropertyFileReader().getPropValues();
+        DiscordApi api = new DiscordApiBuilder().setToken(token).login().join();
 
         api.addMessageCreateListener(event -> {
             if (event.getMessageContent().equalsIgnoreCase("!ping")) {
